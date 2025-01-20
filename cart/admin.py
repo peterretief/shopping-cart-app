3487077c7c94806d29bb05f5d9513b46ba24cac6
Peter Retief <peterretief@gmail.com>
2025-01-20 12:00:51 +0200

    modified:   cart/__pycache__/admin.cpython-312.pyc 	modified:   cart/admin.py 	modified:   cart/templates/cart/vegetable_list.html 	modified:   db.sqlite3

--- cart/admin.py
+++ cart/admin.py
@@ -1,6 +1,8 @@
 # Register your models here.
 from django.contrib import admin
-from .models import Vegetable
+from .models import Vegetable, Order, OrderItem
 
 # Register your models here.
 admin.site.register(Vegetable)
+admin.site.register(Order)
+#admin.site.register(OrderItem
--- veg_shop/urls.py
+++ veg_shop/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from cart import views as cart_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('', include('cart.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
 #    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
@@ -20,3 +23,6 @@ urlpatterns = [
 
     path('accounts/register/', cart_views.register, name='register'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
